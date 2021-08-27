f5790ddfc1f501ee4e12180dbcb92dcdf121bae2
Alejandro Obando Gil <alejo.obando.g@gmail.com>
2021-08-27 12:52:09 -0500

    new branch and add dark

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -119,8 +119,7 @@ class PostsTableSeeder extends Seeder
         $post ->url = str_slug("The Godfather");
         $post ->description = "Película de drama y gansters";
         $post ->body = "El padrino (título original en inglés: The Godfather1​) es una película estadounidense de 1972 dirigida por Francis Ford Coppola. La película fue producida por Albert S. Ruddy, de la compañía Paramount Pictures. Está basada en la novela homónima (que a su vez está basada en la familia real de los Mortillaro de Sicilia, Italia), de Mario Puzo, quien adaptó el guion junto a Coppola y Robert Towne, este último sin ser acreditado.
-
-        ​Protagonizada por Marlon Brando y Al Pacino como los líderes de una poderosa familia criminal ficticia de Nueva York, la historia, ambientada desde 1945 a 1955, cuenta las crónicas de la Familia Corleone liderada por Vito Corleone (Brando), enfocándose en el personaje de Michael Corleone (Pacino), y su transformación de un reacio joven ajeno a los asuntos familiares a un implacable jefe de la mafia ítalo-estadounidense";
+            ​Protagonizada por Marlon Brando y Al Pacino como los líderes de una poderosa familia criminal ficticia de Nueva York, la historia, ambientada desde 1945 a 1955, cuenta las crónicas de la Familia Corleone liderada por Vito Corleone (Brando), enfocándose en el personaje de Michael Corleone (Pacino), y su transformación de un reacio joven ajeno a los asuntos familiares a un implacable jefe de la mafia ítalo-estadounidense";
         $post->published_at = '1972-03-14'; //Carbon::now();
         $post->iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/sY1S34973zA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         $post->category_id = 1;
@@ -139,10 +138,8 @@ class PostsTableSeeder extends Seeder
 
         $post ->description = "Serie de drama y gansters";
         $post ->body = "Peaky Blinders es una serie de televisión inglesa de drama histórico, emitida por el canal BBC Two. La serie está protagonizada por Cillian Murphy y se centra en una familia de gánsteres de Birmingham, durante los años veinte y del ascenso de su jefe, Thomas Shelby.
-
-        Los creadores de la serie se basaron en los Peaky Blinders, una banda criminal que existió en la ciudad de Birmingham a mediados del siglo XX y que se caracterizaba porque cosía hojas de afeitar en sus gorras.1​
-
-        Está producida por Caryn Mandabach Productions y Tiger Aspect Productions y comenzó su emisión en septiembre de 2013.";
+            Los creadores de la serie se basaron en los Peaky Blinders, una banda criminal que existió en la ciudad de Birmingham a mediados del siglo XX y que se caracterizaba porque cosía hojas de afeitar en sus gorras.
+            Está producida por Caryn Mandabach Productions y Tiger Aspect Productions y comenzó su emisión en septiembre de 2013.";
         $post->published_at = '2013-09-12'; //Carbon::now();
         $post->iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/oVzVdvGIC7U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         $post->category_id = 2;
@@ -160,8 +157,7 @@ class PostsTableSeeder extends Seeder
 
         $post ->description = "Película de drama y comedia negra";
         $post ->body = "El lobo de Wall Street —cuyo título original en inglés es The Wolf of Wall Street— es una película de 2013 dirigida por Martin Scorsese, basada en las memorias del mismo nombre de Jordan Belfort. Escrita por Terence Winter y protagonizada por Leonardo DiCaprio como Belfort, junto a Jonah Hill, Margot Robbie, Jean Dujardin, Rob Reiner, Kyle Chandler y Matthew McConaughey, se trata de la quinta colaboración entre Scorsese y DiCaprio.3​
-
-        La película cuenta la historia de un agente de bolsa de Nueva York, interpretado por DiCaprio, que se niega a cooperar en un caso de fraude de títulos que involucra la corrupción en Wall Street, el banco comercial e infiltración de la mafia.";
+            La película cuenta la historia de un agente de bolsa de Nueva York, interpretado por DiCaprio, que se niega a cooperar en un caso de fraude de títulos que involucra la corrupción en Wall Street, el banco comercial e infiltración de la mafia.";
         $post->published_at = '2014-01-24'; //Carbon::now();
         $post->iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/DEMZSa0esCU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         $post->category_id = 4;
@@ -170,5 +166,25 @@ class PostsTableSeeder extends Seeder
         $post->save();
         $post->tags()->attach(2);
         $post->tags()->attach(4);
+        $post->tags()->attach(3);
+
+
+        $post = new Post;
+        // llamado a los valores de la BD
+        $post ->title = "Dark";
+        $post ->url = str_slug("Dark");
+
+        $post ->description = "Serie de suspenso y ciencia ficción";
+        $post ->body = "Dark es una serie de televisión web alemana de suspense y ciencia ficción creada por Baran bo Odar y Jantje Friese.5​6​7​ Situada en la ficticia ciudad de Winden (Alemania), Dark sigue las secuelas de la desaparición de un niño que expone los secretos y las conexiones ocultas entre cuatro familias mientras desentrañan lentamente una siniestra conspiración de viaje en el tiempo que abarca tres generaciones. A lo largo de la serie, Dark explora las implicaciones existenciales del tiempo y sus efectos sobre la naturaleza humana.
+            Dark, la primera serie original de Netflix en idioma alemán; se estrenó en el servicio de streaming Netflix el 1 de diciembre de 2017. La primera temporada fue recibida con reseñas positivas de los críticos, que hicieron comparaciones iniciales con Stranger Things, otra serie de Netflix.";
+        $post->published_at = '2017-12-01'; //Carbon::now();
+        $post->iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/IJ_AZCvCacw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        $post->category_id = 6;
+        $post->user_id = 2;
+        // guardar en la BD
+        $post->save();
+        $post->tags()->attach(6);
+        $post->tags()->attach(9);
+        $post->tags()->attach(8);
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -39,12 +39,32 @@
                 </div>
             </div>
         </div>
-        @if($post->iframe)
-            <div class="video" style="padding-top:25px">
-                {!! $post->iframe !!}
-            </div>
-        @endif
+        <div class="trailer">
+            <h3>Trailer</h3>
+            @if($post->iframe)
+                <div class="video" style="padding-top:25px">
+                    {!! $post->iframe !!}
+                </div>
+            @endif
+        </div>
 
+        temporada 1
+        cap 1
+        <iframe src="https://gcs.megaplay.cc/index.php?h=WEdZM1RiREEzSEFhZEJhQXVSenBjL3d3L00rbUkxL1RuSzBJQ2FKRzBJaDh2cGlNU1BsUHVnTUZwRnZIVHdjZw==&bg=/image.tmdb.org/t/p/w780/l3mOi1hYf7SMmO28SJ69ynCDpvW.jpg" width="740" height="480"></iframe>
+        temporada 1
+        cap 2
+        <iframe src="https://gcs.megaplay.cc/index.php?h=WEdZM1RiREEzSEFhZEJhQXVSenBjNFFFVjNqWnpyT1Z0cXZLNUIxMEdyS3Q4ODlJN0pJSzBWUDd0dTlFdkN1Tg==&bg=/image.tmdb.org/t/p/w780/d3c3iXv04bPmTv0V9QTGew0n36k.jpg" width="740" height="480"></iframe>
+        temporada 1
+        cap 3
+        <iframe src="https://gcs.megaplay.cc/index.php?h=WEdZM1RiREEzSEFhZEJhQXVSenBjMGZyYU1VRjIzVzVlQ0x4WVJPRUNQcW5CYk41Q3lUbW9BMHZqMGN2VTI1ZA==&bg=/image.tmdb.org/t/p/w780/jG5jjxTcpvxn74cY4LFIzeqGph3.jpg" width="740" height="480"></iframe>
+        temporada 1
+        cap 4
+        <iframe src="https://gcs.megaplay.cc/index.php?h=WEdZM1RiREEzSEFhZEJhQXVSenBjeFIvTmFRa09vb0JmZnZRcUFlWi82dzJDclNVVEJFUGIxV3AxTUpGMVJpaw==&bg=/image.tmdb.org/t/p/w780/2BPHWlIVes7uNniuM5jyoflAbap.jpg" width="740" height="480"></iframe>
+        temporada 1
+        cap 5
+        <iframe src="https://gcs.megaplay.cc/index.php?h=WEdZM1RiREEzSEFhZEJhQXVSenBjMzRDMUlMNEM5UXR3Y0RyKzVFdmRYdFZob0NYdDVWQkJJOWpnUlhodzRGYg==&bg=/image.tmdb.org/t/p/w780/piwRYraJ1jUE8pcuQJfWPgs6hdy.jpg" width="740" height="480"></iframe>
+
+        {{-- <iframe id="iframe" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" src="https://gcs.megaplay.cc/index.php?h=WEdZM1RiREEzSEFhZEJhQXVSenBjL3d3L00rbUkxL1RuSzBJQ2FKRzBJaDh2cGlNU1BsUHVnTUZwRnZIVHdjZw==&amp;bg=/image.tmdb.org/t/p/w780/l3mOi1hYf7SMmO28SJ69ynCDpvW.jpg"></iframe> --}}
 {{-- ---------------------------------------FOOTER------------------------------- --}}
         <footer class="container-flex space-between">
             <div class="tags container-flex" style="padding-top: 20px">
